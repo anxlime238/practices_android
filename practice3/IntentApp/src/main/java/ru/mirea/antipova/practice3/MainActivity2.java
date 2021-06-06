0d1f90672d873199bde3f64450fbe896eedaafab
anxlime238 <anxlime@gmail.com>
2021-06-06 17:04:57 +0300

    practice3

---
+++ practice3/IntentApp/src/main/java/ru/mirea/antipova/practice3/MainActivity2.java
@@ -0,0 +1,35 @@
+package ru.mirea.antipova.practice3;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import ru.mirea.antipova.practice3.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String string = intent.getStringExtra("date");
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(string);
+    }
+}
